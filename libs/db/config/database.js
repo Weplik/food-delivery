75a1a74c3a644ed87118695c31d8ef1032a82951
Weplik <krylov.denis.93@gmail.com>
2019-11-26 20:04:13 +0600

    add docker-compose

--- libs/db/config/database.js
+++ libs/db/config/database.js
@@ -2,18 +2,19 @@ const { NODE_ENV } = require('./constants');
 
 const config = {
   development: {
-    username: 'postgres',
-    password: 'postgres',
-    database: 'food_delivery_db',
-    host: '127.0.0.1',
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
   },
   production: {
-    username: 'postgres',
-    password: 'postgres',
-    database: 'food_delivery_db',
-    host: '127.0.0.1',
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
+    logging: false,
   },
 };
 
