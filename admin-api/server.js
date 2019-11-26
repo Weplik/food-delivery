bfc79d96a16079a27c6c0ee11ed30b6625b3bfdd
Weplik <krylov.denis.93@gmail.com>
2019-11-26 19:26:35 +0600

    add logger to admin-api

--- admin-api/server.js
+++ admin-api/server.js
@@ -1,10 +1,11 @@
 const { isConnectedToDB } = require('../libs/db/models');
+const logger = require('./helpers/logger');
 
 isConnectedToDB()
   .then(() => {
     const app = require('./config/app');
     const { APP_PORT } = require('./config/constants');
 
-    app.listen(APP_PORT, () => console.log('Application successfully started'));
+    app.listen(APP_PORT, () => logger.info('Application successfully started'));
   })
-  .catch(err => console.error(err.message));
+  .catch(err => logger.error(err.message));
